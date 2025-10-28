dd244aad674ee17526bcf9bb6e2346ae0c846e11
Maysara <maysaracs1001@gmail.com>
2025-10-28 15:59:37 +0300

    project structure adjusted

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI project',
+    version = '0.0.0',
+    auther = 'Maysara Moahmed',
+    auther_email = 'maysaracs1001@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
